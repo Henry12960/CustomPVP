8de71c3341cbdb779ff4abe122b6714f23fac341
Ihsan65 <107484695+Ihsan65@users.noreply.github.com>
2022-06-18 19:46:06 +0700

    Uh

--- src/CustomPVP/Main.php
+++ src/CustomPVP/Main.php
@@ -15,19 +15,14 @@ class Main extends PluginBase implements Listener {
 	private static $instance;
 	
 	public function onEnable() : void {
-		
 		$this->getLogger()->info(TextFormat::colorize("&aCustom PVP By HenryDM"));
-		
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-		
 		$this->saveResource("config.yml");
-		
+    		self::$instance = $this;
 	}
 	
 	public static function getInstance() : Main {
-		
 		return self::$instance;
 		
 	}
